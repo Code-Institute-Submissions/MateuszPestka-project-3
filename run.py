71dc8cdf3d3f60fb8bc2e775d211a0557abd2325
MateuszPestka <pes.matthew@gmail.com>
2022-04-14 15:22:08 +0000

    Add print board function

--- run.py
+++ run.py
@@ -2,4 +2,9 @@
 
 board = [["-" for r in range(3)] for c in range(3)]
 
-print(board)
+def print_board(board):
+    for row in board:
+        for slot in board:
+            print(slot)
+
+print_board(board)
