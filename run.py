291f52ff4a1121df5f3141517461d2624fd30257
MateuszPestka <pes.matthew@gmail.com>
2022-04-14 15:32:10 +0000

    Fix bug where 9 rows are printed

--- run.py
+++ run.py
@@ -7,8 +7,8 @@ def print_board(board):
     Makes the board for the game
     """
     for row in board:
-        for slot in board:
-            print(f"{slot}", end="")
-        print()
+        for slot in row:
+            print(f"{slot} ", end="")
+        print() # stops grid from lining up in a single line
 
 print_board(board)
